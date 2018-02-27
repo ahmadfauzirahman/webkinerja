42d3d47ce5e52fc2c3b8067bec265fd79e4b0202
alief_alhadi <aliefalhadi28@gmail.com>
2018-02-27 14:34:53 +0700

    Alif Punya

---
+++ common/models/WebEvents.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "web_events".
+ *
+ * @property int $eventsID
+ * @property string $eventsJudul
+ * @property string $eventsTanggalMulai
+ * @property string $eventsTanggalSelesai
+ * @property string $eventsLokasi
+ * @property string $eventsKeterangan
+ * @property string $eventsThumbnails
+ * @property string $eventsStatus
+ */
+class WebEvents extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'web_events';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['eventsJudul', 'eventsLokasi', 'eventsKeterangan', 'eventsThumbnails'], 'string'],
+            [['eventsTanggalMulai', 'eventsTanggalSelesai'], 'safe'],
+            [['eventsStatus'], 'string', 'max' => 50],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'eventsID' => 'Events ID',
+            'eventsJudul' => 'Events Judul',
+            'eventsTanggalMulai' => 'Events Tanggal Mulai',
+            'eventsTanggalSelesai' => 'Events Tanggal Selesai',
+            'eventsLokasi' => 'Events Lokasi',
+            'eventsKeterangan' => 'Events Keterangan',
+            'eventsThumbnails' => 'Events Thumbnails',
+            'eventsStatus' => 'Events Status',
+        ];
+    }
+}
